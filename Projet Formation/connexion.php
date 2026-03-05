c625d7dd687ca9ae1263b9054917034af3178dc2
Rxm34 <remy.borde34@gmail.com>
2026-03-05 14:27:17 +0100

    modification connexion projet formation

--- Projet Formation/connexion.php
+++ Projet Formation/connexion.php
@@ -1,8 +1,8 @@
 <?php
-    $host = 'localhost';
-    $bdd = 'GestionFormation';
-    $user = 'root';
-    $passwd = 'root';
+    $host   = $_ENV['DB_HOST'];
+    $bdd    = $_ENV['DB_NAME'];
+    $user   = $_ENV['DB_USER'];
+    $passwd = $_ENV['DB_PASS'];
     try {
         $cnn = new PDO("mysql:host=$host;dbname=$bdd;charset=utf8", $user, $passwd , array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
