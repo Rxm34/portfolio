3d234286a1e39c0f3053b089e7b0b9782a8da0f3
Rxm34 <remy.borde34@gmail.com>
2026-03-05 14:54:57 +0100

    modification index

--- Projet Formation/index.php
+++ Projet Formation/index.php
@@ -7,29 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="TemplateMo">
-  
+ 
     <title>EduTech</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 
-    <!-- Additional CSS Files -->
     <link rel="stylesheet" href="assets/css/fontawesome.css">
     <link rel="stylesheet" href="assets/css/templatemo-stand-blog.css">
     <link rel="stylesheet" href="assets/css/owl.css">
-<!--
-
-TemplateMo 551 Stand Blog
-
-https://templatemo.com/tm-551-stand-blog
-
--->
-  </head>
+</head>
 
   <body id="accueil">
 
-    <!-- ***** Preloader Start ***** -->
     <div id="preloader">
         <div class="jumper">
             <div></div>
@@ -37,9 +27,6 @@ https://templatemo.com/tm-551-stand-blog
             <div></div>
         </div>
     </div>  
-    <!-- ***** Preloader End ***** -->
-
-    <!-- Header -->
     <header class="">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
@@ -66,9 +53,6 @@ https://templatemo.com/tm-551-stand-blog
       </nav>
     </header>
 
-    <!-- Page Content -->
-    <!-- Banner Starts Here -->
-
     <section class="blog-posts">
       <div class="container">
         <div class="row">
@@ -88,54 +72,43 @@ https://templatemo.com/tm-551-stand-blog
         </div>
       </div>
     </section>
-    <?php 
-      include('connexion.php');
-      $resultat = $cnn->prepare("SELECT * FROM THEME");
-      $resultat->execute();
-      // Affichage des lignes. Une ligne contient un enregistrement
-      while($ligne = $resultat->fetch()) {
-    ?>           
 
     <section class="blog-posts">
-    <h2>Bienvenue chez EduTech</h2><br><br>
-    <?php echo '<p>EduTech est une entreprise spécialisée dans les formations informatiques.</p>';
-          echo '<p>Que vous soyez débutant ou professionnel, nos parcours sont conçus pour </p>';
-          echo '<p>tous les niveaux.</p><br><br>';
-          echo '<h3> Veuillez choisir le thème de votre prochaine formation.</h3>'?>
-    <div class="main-banner header-text">
-      <div class="container-fluid">
-        <div class="owl-banner owl-carousel">
-        
-          
- <?php 
-      include('connexion.php');
-      $resultat = $cnn->prepare("SELECT * FROM THEME");
-      $resultat->execute();
-      // Affichage des lignes. Une ligne contient un enregistrement
-      while($ligne = $resultat->fetch()) {
-    ?>           
-        <a href="formation.php?id=<?php echo $ligne['IdTheme']; ?>"><div class="item">
-            <img src="assets/images/<?php echo $ligne['logotheme']; ?>" alt="" >
-            <div class="item-content">
-              <div class="main-content">
-                <div class="meta-category">
-                    <?php echo '<span>'.$ligne['NomTheme'].'</span>'; ?> 
+        <div class="container">
+            <h2>Bienvenue chez EduTech</h2><br><br>
+            <p>EduTech est une entreprise spécialisée dans les formations informatiques.</p>
+            <p>Que vous soyez débutant ou professionnel, nos parcours sont conçus pour tous les niveaux.</p><br><br>
+            <h3>Veuillez choisir le thème de votre prochaine formation.</h3>
+            
+            <div class="main-banner header-text">
+                <div class="container-fluid">
+                    <div class="owl-banner owl-carousel">
+                        <?php 
+                            include('connexion.php');
+                            $resultat = $cnn->prepare("SELECT * FROM THEME");
+                            $resultat->execute();
+                            // Affichage des lignes
+                            while($ligne = $resultat->fetch()) {
+                        ?>           
+                            <a href="formation.php?id=<?php echo $ligne['IdTheme']; ?>">
+                                <div class="item">
+                                    <img src="assets/images/<?php echo $ligne['logotheme']; ?>" alt="">
+                                    <div class="item-content">
+                                        <div class="main-content">
+                                            <div class="meta-category">
+                                                <span><?php echo $ligne['NomTheme']; ?></span> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        <?php
+                            }
+                        ?>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </a>
-       
-
-  <?php
-      }
-     ?>
-     <?php
-      }
-     ?>
         </div>
-      </div>
-    </div>
     </section>
     
     <footer>
@@ -151,11 +124,9 @@ https://templatemo.com/tm-551-stand-blog
       </div>
     </footer>
 
-    <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Additional Scripts -->
     <script src="assets/js/custom.js"></script>
     <script src="assets/js/owl.js"></script>
     <script src="assets/js/slick.js"></script>
@@ -163,11 +134,11 @@ https://templatemo.com/tm-551-stand-blog
     <script src="assets/js/accordions.js"></script>
 
     <script language = "text/Javascript"> 
-      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
-      function clearField(t){                   //declaring the array outside of the
-      if(! cleared[t.id]){                      // function makes it static and global
-          cleared[t.id] = 1;  // you could use true and false, but that's more typing
-          t.value='';         // with more chance of typos
+      cleared[0] = cleared[1] = cleared[2] = 0; 
+      function clearField(t){                   
+      if(! cleared[t.id]){                     
+          cleared[t.id] = 1;  
+          t.value='';         
           t.style.color='#fff';
           }
       }
